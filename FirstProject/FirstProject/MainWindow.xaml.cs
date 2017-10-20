1d777ba93230b6193df4f5e9e19c3d911660e784
Andre Luiz <andr234ibatera@gmail.com>
2017-10-19 22:01:45 -0400

    Primeiro dia
    Primeiro dia sem avanços em capturar as imagens coloridas

--- FirstProject/FirstProject/MainWindow.xaml.cs
+++ FirstProject/FirstProject/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Kinect;
+
 namespace FirstProject
 {
     /// <summary>
@@ -20,9 +22,90 @@ namespace FirstProject
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private KinectSensor kinectSensor = KinectSensor.GetDefault();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnStram_Click(object sender, RoutedEventArgs e)
+        {
+            if (kinectSensor.IsOpen)
+            {
+                kinectSensor.Close();
+                labelStatus.Content = "Kinect Desconnected";
+            }
+            else
+            {
+                kinectSensor.Open();
+                labelStatus.Content = "Kinect Connected";
+
+                ColorFrameReader frameReader = kinectSensor.ColorFrameSource.OpenReader();
+                TypedReference reference = new TypedReference
+                //frameReader.FrameArrived += 
+
+                //MultiSourceFrameReader frame = kinectSensor.OpenMultiSourceFrameReader(
+                //FrameSourceTypes.Color);
+                // | FrameSourceTypes.Depth | FrameSourceTypes.Infrared
+
+                
+            }
+        }
+
+        //void MultiReader(object sender, MultiSourceFrameArrivedEventArgs eventArgs)
+        //{
+        //    var reference = eventArgs.FrameReference.AcquireFrame();
+
+        //    //color frame
+        //    using (var frame = reference.ColorFrameReference.AcquireFrame())
+        //    {
+        //        if (frame != null)
+        //        {
+
+        //        }
+        //    }
+
+        //    //depth frame
+        //    using (var frame = reference.DepthFrameReference.AcquireFrame())
+        //    {
+        //        if (frame != null)
+        //        {
+
+        //        }
+        //    }
+
+        //    //infrared frame
+        //    using (var frame = reference.InfraredFrameReference.AcquireFrame())
+        //    {
+        //        if (frame != null)
+        //        {
+
+        //        }
+        //    }
+        //}
+
+        //private ImageSource ToBitmap(ColorFrame colorFrame)
+        //{
+        //    int width = colorFrame.FrameDescription.Width;
+        //    int height = colorFrame.FrameDescription.Height;
+
+        //    byte[] pixels = new byte[width*height*((PixelFormats.Bgr32.BitsPerPixel +7)/8)];
+
+        //    if (colorFrame.RawColorImageFormat == ColorImageFormat.Bgra)
+        //    {
+        //        colorFrame.CopyRawFrameDataToArray(pixels);
+        //    }
+        //    else
+        //    {
+        //        colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+        //    }
+
+        //    int stride = width * PixelFormats.Bgr32.BitsPerPixel / 8;
+
+        //    return BitmapSource.Create(width,height,StreamScreen.Width,
+        //        StreamScreen.Height,PixelFormats.Bgr32,null,pixels,stride);
+        //}
     }
 }
